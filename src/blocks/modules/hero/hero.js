72abcec88773295d7a7be0ef0b3d26df0cb6974d
KostaSenatov <kostyasenatov@gmail.com>
2022-09-16 20:13:04 +0700

    .

--- src/blocks/modules/hero/hero.js
+++
@@ -1,4 +0,0 @@
-BackgroundCheck.init({
-	targets: '.hero__title',
-	images: '.hero__video'
-});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,7 +1,6 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
 
-import "%modules%/hero/hero";
 import "%modules%/realization/realization";
 import "%modules%/wehelp/wehelp";
 import "%modules%/confirm/confirm";
