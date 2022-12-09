5d3d3bc8d36a39903162d22c2def4422563ed2da
Hudson8811 <hudson88@yandex.ru>
2022-12-09 13:48:35 +0300

    server version

--- src/blocks/modules/wehelp/wehelp.js
+++ src/blocks/modules/wehelp/wehelp.js
@@ -1,20 +1,22 @@
-var swiper = new Swiper(".wehelp__swiper", {
-	loop:true,
-	speed:3000,
-	slidesPerView: "1.9",
-	autoplay: {
-		delay: 0,
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: "3",
-		},
-		1024: {
-			slidesPerView: "3.8",
-		},
-	}
-});
+(function ($) {
+  var swiper = new Swiper(".wehelp__swiper", {
+    loop: true,
+    speed: 3000,
+    slidesPerView: "1.9",
+    autoplay: {
+      delay: 0,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: "3",
+      },
+      1024: {
+        slidesPerView: "3.8",
+      },
+    }
+  });
 
-$('.form-wewillfind-footer__item input').focus(function() {
-	$(this).parent().addClass("js-active-area")
-});
+  $('.form-wewillfind-footer__item input').focus(function () {
+    $(this).parent().addClass("js-active-area")
+  });
+})(jQuery);
