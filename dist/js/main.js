223599d45386f46282fe7cef4e358083b5f7ad9c
Hudson8811 <hudson88@yandex.ru>
2022-12-18 16:05:29 +0300

    fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -330,19 +330,16 @@ __webpack_require__.r(__webpack_exports__);
     });
     /* 	var $animation_elements = $('.link1');
     	var $window = $(window);
-    
-    	function check_if_in_view() {
+    		function check_if_in_view() {
     		var window_height = $window.height();
     		var window_top_position = $window.scrollTop();
     		var window_bottom_position = (window_top_position + window_height);
-    
-    		$.each($animation_elements, function() {
+    			$.each($animation_elements, function() {
     			var $element = $(this);
     			var element_height = $element.outerHeight();
     			var element_top_position = $element.offset().top;
     			var element_bottom_position = (element_top_position + element_height);
-    
-    			//check to see if this current container is within viewport
+    				//check to see if this current container is within viewport
     			if ((element_bottom_position >= window_top_position) &&
     				(element_top_position <= window_bottom_position)) {
     				$element.addClass('is-visible');
@@ -351,8 +348,7 @@ __webpack_require__.r(__webpack_exports__);
     			}
     		});
     	}
-    
-    	$window.on('scroll resize', check_if_in_view);
+    		$window.on('scroll resize', check_if_in_view);
     	$window.trigger('scroll'); */
   });
   var page__image = gsap.timeline({
