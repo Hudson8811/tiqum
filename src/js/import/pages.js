e35e4c7513a3dc03ecb1ef8b2b14fc0f2b6d8e4c
kihhmaster <kihhmaster@yandex.ru>
2023-04-04 17:57:50 +0300

    Правки index.html

--- src/js/import/pages.js
+++ src/js/import/pages.js
@@ -192,5 +192,18 @@
 
     });
 
-})(jQuery);
+		
+    $('.js-services__item').hover(function (){
+			$(this).siblings('.js-services__item').addClass('opacity');
+		},function (){
+				$('.js-services__item').removeClass('opacity');
+		});
+
+
+				// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+		let vh = window.innerHeight * 0.01;
+		// Then we set the value in the --vh custom property to the root of the document
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	})(jQuery);
 
