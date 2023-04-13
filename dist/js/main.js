308833b845ad390048f67e92357d95a711e48adc
kihhmaster <kihhmaster@yandex.ru>
2023-04-13 17:04:22 +0300

    Правки от 13.04.23

--- dist/js/main.js
+++ dist/js/main.js
@@ -255,21 +255,21 @@ var swiper = new Swiper(".realization__swiper", {
   \*************************************************/
 /***/ (function() {
 
-(function ($) {
-  var windowInnerWidth = window.innerWidth;
-  console.log(windowInnerWidth);
-  if (windowInnerWidth > 1024) {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.utils.toArray(".services__items").forEach(function (panel, i) {
-      ScrollTrigger.create({
-        trigger: panel,
-        start: 'bottom bottom',
-        pin: true,
-        pinSpacing: false
-      });
-    });
-  }
-})(jQuery);
+// (function ($) {
+//   const windowInnerWidth = window.innerWidth
+//   console.log(windowInnerWidth);
+//   if (windowInnerWidth > 1024) {
+//     gsap.registerPlugin(ScrollTrigger);
+//     gsap.utils.toArray(".services__items").forEach((panel, i) => {
+//       ScrollTrigger.create({
+//         trigger: panel,
+//         start: 'bottom bottom',
+//         pin: true,
+//         pinSpacing: false
+//       });
+//     });
+//   }
+// })(jQuery);
 
 /***/ }),
 
--- src/blocks/modules/services/services.js
+++ src/blocks/modules/services/services.js
@@ -1,15 +1,15 @@
-(function ($) {
-  const windowInnerWidth = window.innerWidth
-  console.log(windowInnerWidth);
-  if (windowInnerWidth > 1024) {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.utils.toArray(".services__items").forEach((panel, i) => {
-      ScrollTrigger.create({
-        trigger: panel,
-        start: 'bottom bottom',
-        pin: true,
-        pinSpacing: false
-      });
-    });
-  }
-})(jQuery);
+// (function ($) {
+//   const windowInnerWidth = window.innerWidth
+//   console.log(windowInnerWidth);
+//   if (windowInnerWidth > 1024) {
+//     gsap.registerPlugin(ScrollTrigger);
+//     gsap.utils.toArray(".services__items").forEach((panel, i) => {
+//       ScrollTrigger.create({
+//         trigger: panel,
+//         start: 'bottom bottom',
+//         pin: true,
+//         pinSpacing: false
+//       });
+//     });
+//   }
+// })(jQuery);
