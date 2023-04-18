5fd00ab5f195fcceb1af8c00295072a4c4ede0e3
kihhmaster <kihhmaster@yandex.ru>
2023-04-18 11:08:38 +0300

    Спасибо за заявку

--- dist/js/main.js
+++ dist/js/main.js
@@ -256,19 +256,19 @@ var swiper = new Swiper(".realization__swiper", {
 /***/ (function() {
 
 // (function ($) {
-//   const windowInnerWidth = window.innerWidth
-//   console.log(windowInnerWidth);
-//   if (windowInnerWidth > 1024) {
-//     gsap.registerPlugin(ScrollTrigger);
-//     gsap.utils.toArray(".services__items").forEach((panel, i) => {
-//       ScrollTrigger.create({
-//         trigger: panel,
-//         start: 'bottom bottom',
-//         pin: true,
-//         pinSpacing: false
-//       });
-//     });
-//   }
+//     const windowInnerWidth = window.innerWidth
+//     console.log(windowInnerWidth);
+//     if (windowInnerWidth > 1024) {
+//         gsap.registerPlugin(ScrollTrigger);
+//         gsap.utils.toArray(".services__items").forEach((panel, i) => {
+//             ScrollTrigger.create({
+//                 trigger: panel,
+//                 start: 'bottom bottom',
+//                 pin: true,
+//                 pinSpacing: false
+//             });
+//         });
+//     }
 // })(jQuery);
 
 /***/ }),
