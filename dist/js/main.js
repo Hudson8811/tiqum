421ccc8cc5d61076ba307e7dce203999172b857b
PipZip <37911683+PipZip@users.noreply.github.com>
2023-05-22 21:29:17 +0600

    footer form

--- dist/js/main.js
+++ dist/js/main.js
@@ -29,6 +29,73 @@
 /***/ (function() {
 
 (function ($) {
+  var Defaults = $.fn.select2.amd.require('select2/defaults');
+  $.extend(Defaults.defaults, {
+    dropdownPosition: 'auto'
+  });
+  var AttachBody = $.fn.select2.amd.require('select2/dropdown/attachBody');
+  var _positionDropdown = AttachBody.prototype._positionDropdown;
+  AttachBody.prototype._positionDropdown = function () {
+    var $window = $(window);
+    var isCurrentlyAbove = this.$dropdown.hasClass('select2-dropdown--above');
+    var isCurrentlyBelow = this.$dropdown.hasClass('select2-dropdown--below');
+    var newDirection = null;
+    var offset = this.$container.offset();
+    offset.bottom = offset.top + this.$container.outerHeight(false);
+    var container = {
+      height: this.$container.outerHeight(false)
+    };
+    container.top = offset.top;
+    container.bottom = offset.top + container.height;
+    var dropdown = {
+      height: this.$dropdown.outerHeight(false)
+    };
+    var viewport = {
+      top: $window.scrollTop(),
+      bottom: $window.scrollTop() + $window.height()
+    };
+    var enoughRoomAbove = viewport.top < offset.top - dropdown.height;
+    var enoughRoomBelow = viewport.bottom > offset.bottom + dropdown.height;
+    var css = {
+      left: offset.left,
+      top: container.bottom
+    };
+
+    // Determine what the parent element is to use for calciulating the offset
+    var $offsetParent = this.$dropdownParent;
+
+    // For statically positoned elements, we need to get the element
+    // that is determining the offset
+    if ($offsetParent.css('position') === 'static') {
+      $offsetParent = $offsetParent.offsetParent();
+    }
+    var parentOffset = $offsetParent.offset();
+    css.top -= parentOffset.top;
+    css.left -= parentOffset.left;
+    var dropdownPositionOption = this.options.get('dropdownPosition');
+    if (dropdownPositionOption === 'above' || dropdownPositionOption === 'below') {
+      newDirection = dropdownPositionOption;
+    } else {
+      if (!isCurrentlyAbove && !isCurrentlyBelow) {
+        newDirection = 'below';
+      }
+      if (!enoughRoomBelow && enoughRoomAbove && !isCurrentlyAbove) {
+        newDirection = 'above';
+      } else if (!enoughRoomAbove && enoughRoomBelow && isCurrentlyAbove) {
+        newDirection = 'below';
+      }
+    }
+    if (newDirection == 'above' || isCurrentlyAbove && newDirection !== 'below') {
+      css.top = container.top - parentOffset.top - dropdown.height;
+    }
+    if (newDirection != null) {
+      this.$dropdown.removeClass('select2-dropdown--below select2-dropdown--above').addClass('select2-dropdown--' + newDirection);
+      this.$container.removeClass('select2-container--below select2-container--above').addClass('select2-container--' + newDirection);
+    }
+    this.$dropdownContainer.css(css);
+  };
+})(window.jQuery);
+(function ($) {
   $(".calc__link--circle").click(function () {
     $(".modalFromFooter").addClass("active");
     $("body").addClass("noScroll");
@@ -65,7 +132,7 @@
           $(document).on('click', '.form-wewillfind-footer button[type=submit]', function () {
             event.preventDefault();
             var valid = true;
-            $(this).closest('form').find('input:not([type=file]), textarea').each(function () {
+            $(this).closest('form').find('input:not([type=file]), textarea, select').each(function () {
               if (validInput(this)) {
                 $(this).closest('.form-wewillfind-footer__item').addClass("js-active-area").removeClass("js-error");
               } else {
@@ -107,7 +174,7 @@
     $(document).on('click', '.form-wewillfind-footer button[type=submit]', function () {
       event.preventDefault();
       var valid = true;
-      $(this).closest('form').find('input:not([type=file]), textarea').each(function () {
+      $(this).closest('form').find('input:not([type=file]), textarea, select').each(function () {
         if (validInput(this)) {
           $(this).closest('.form-wewillfind-footer__item').addClass("js-active-area").removeClass("js-error");
         } else {
@@ -119,6 +186,20 @@
         //$(this).closest('form').submit();
         //submit code here
         $(this).closest('form').addClass('submited');
+      }
+    });
+  }
+  if ($(".buget-select").length > 0) {
+    $(".buget-select").select2({
+      placeholder: "Бюджет проекта",
+      allowClear: false,
+      minimumResultsForSearch: Infinity,
+      dropdownPosition: 'below'
+    });
+    $(document).on('change', ".buget-select", function () {
+      var select = this;
+      if (validInput(select)) {
+        $(select).closest('.form-wewillfind-footer__item').addClass("js-active-area").removeClass("js-error");
       }
     });
   }
