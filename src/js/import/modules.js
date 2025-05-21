4826585b0537419c9238daecfd119c780a7cef0f
FrontMD <web@mockup.digital>
2025-05-21 16:17:44 +0300

    awards

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,7 +2,7 @@ import "%modules%/header/header";
 import "%modules%/footer/footer";
 
 import "%modules%/realization/realization";
-import "%modules%/wehelp/wehelp";
+//import "%modules%/wehelp/wehelp";
 import "%modules%/confirm/confirm";
 import "%modules%/services/services";
 import "%modules%/weright/weright";
